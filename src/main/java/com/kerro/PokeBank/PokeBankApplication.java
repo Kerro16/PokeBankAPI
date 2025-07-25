eba4f43f0da6cad8845fedbc687c1112f5611162
kerro16 <kerrox16@gmail.com>
2025-07-25 10:21:47 -0600

    Initial Commit

---
+++ src/main/java/com/kerro/PokeBank/PokeBankApplication.java
@@ -0,0 +1,13 @@
+package com.kerro.PokeBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokeBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kerro/PokeBank/PokeBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kerro.PokeBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokeBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
